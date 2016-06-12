c87a7e2c6ed62c968bf89198380f531bf0edc10d
Ryan Dahl <ry@tinyclouds.org>
2016-06-11 18:53:43 -0700

    update config

--- config.py
+++ config.py
@@ -11,6 +11,20 @@ class Config:
             root[k] = v
         self.stack = [ root ]
 
+    def iteritems(self):
+        return self.to_dict().iteritems()
+
+    def to_dict(self):
+        self._pop_stale()
+        out = {}
+        # Work backwards from the flags to top fo the stack
+        # overwriting keys that were found earlier.
+        for i in range(len(self.stack)):
+            cs = self.stack[-i]
+            for name in cs:
+                out[name] = cs[name]
+        return out
+
     def _pop_stale(self):
         var_scope_name = tf.get_variable_scope().name
         top = self.stack[0]
@@ -20,7 +34,7 @@ class Config:
             top = self.stack[0]
 
     def __getitem__(self, name):
-        self._pop_stale()            
+        self._pop_stale()
         # Recursively extract value
         for i in range(len(self.stack)):
             cs = self.stack[i]
@@ -29,8 +43,20 @@ class Config:
 
         raise KeyError(name)
 
+    def set_default(self, name, value):
+        if not name in self:
+            self[name] = value
+
+    def __contains__(self, name):
+        self._pop_stale()
+        for i in range(len(self.stack)):
+            cs = self.stack[i]
+            if name in cs:
+                return True
+        return False
+
     def __setitem__(self, name, value):
-        self._pop_stale()            
+        self._pop_stale()
         top = self.stack[0]
         var_scope_name = tf.get_variable_scope().name
         assert top.contains(var_scope_name)
@@ -67,15 +93,20 @@ if __name__ == '__main__':
     assert c['hello'] == 1
 
     with tf.variable_scope('foo'):
+        c.set_default("bar", 10)
         c['bar'] = 2
         assert c['bar'] == 2
         assert c['hello'] == 1
+
+        c.set_default("mario", True)
 
         with tf.variable_scope('meow'):
             c['dog'] = 3
             assert c['dog'] == 3
             assert c['bar'] == 2
             assert c['hello'] == 1
+
+            assert c['mario'] == True
 
         assert_raises(KeyError, lambda: c['dog'])
         assert c['bar'] == 2
